acd964e252e990ffe3bdd9c5e0aaff9886a00fa4
CatEater00 <fkzpt345@gmail.com>
2024-03-21 23:43:42 +0900

    Week 3 : 박관호 과제 제출

---
+++ 박관호/3주차/Q1.py
@@ -0,0 +1,13 @@
+while 1 :
+    x = int(input("정수 x를 입력하시오."))
+    y = int(input("정수 y를 입력하시오."))
+    if x < 0 and y < 0 :
+        print("자연수가 아닙니다")
+        continue
+    else :
+        print("합", x + y)
+    print("차", x - y)
+    print("곱", x * y)
+    print("나누기", x / y)
+    print("나머지", x % y)
+    break
