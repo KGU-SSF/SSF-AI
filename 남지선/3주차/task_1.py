d780e51672764de82acf70decab8c34cd07e8ab7
njisun <namjiseon348@gmail.com>
2024-03-21 23:13:10 +0900

    Week 3 : 남지선 과제 제출

---
+++ 남지선/3주차/task_1.py
@@ -0,0 +1,12 @@
+num1,num2=input("입력:").split()
+
+num1=int(num1)
+num2=int(num2)
+
+add=num1+num2
+min=num1-num2
+mul=num1*num2
+quo=num1/num2
+rem=num1%num2
+
+print("출력:",add, min, mul, quo, rem)
