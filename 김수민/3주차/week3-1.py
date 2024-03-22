6e156965b2a777d11b244b1f2898554bc6568286
sumin0312 <162525493+sumin0312@users.noreply.github.com>
2024-03-22 15:54:07 +0900

    Update week3-1.py

--- 김수민/3주차/week3-1.py
+++ 김수민/3주차/week3-1.py
@@ -1,8 +1,8 @@
 a=int(input("첫번째 정수 :"))
 b=int(input("두번째 정수 :"))
 
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a%b)
+print(int(a+b))
+print(int(a-b))
+print(int(a*b))
+print(int(a/b))
+print(int(a%b))
