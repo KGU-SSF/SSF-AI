73adf396e75669d5d17f62180abbe6c1abe11c03
BY-PC\bu200 <bu2000kim@gmail.com>
2024-03-22 13:39:01 +0900

    Week 3 : 김부연 과제 제출

---
+++ 김부연/3주차/number2.py
@@ -0,0 +1,12 @@
+score = int(input("시험 점수를 입력하세요: "))
+
+if 90 <=score <=100:
+    print("A")
+elif 80<=score <=89:
+    print("B")
+elif 70<=score <=79:
+    print("C")
+elif 60<=score <=69:
+    print("D")
+else:
+    print("F")
