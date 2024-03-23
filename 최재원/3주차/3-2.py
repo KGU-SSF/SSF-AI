9b209f693ab2dcc1917846e4db04edbe54f57636
Choi jaewon <111185645+jaewon3640@users.noreply.github.com>
2024-03-23 20:44:41 +0900

    Week 3: 최재원

---
+++ 최재원/3주차/3-2.py
@@ -0,0 +1,16 @@
+grade = int(input("점수를 입력하세요 : "))
+
+
+if grade >= 90 and grade <= 100:
+        print("A")
+elif grade >= 80 and grade < 90: #파이썬 && 는 and
+        print("B")
+elif grade >= 70 and grade < 80:
+        print("C")
+elif grade >= 60 and grade < 70:
+        print("D")
+elif grade > 0 and grade < 60:
+    print("F")
+else:
+    print("다시 입력해주세요.")
+        
