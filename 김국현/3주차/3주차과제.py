41b588c8b90379b0d5b064a8887f45396ae20ab3
guk4979 <guk5871@gmail.com>
2024-03-23 17:42:27 +0900

    Week 3 : 김국현 과제 제출

---
+++ 김국현/3주차/3주차과제.py
@@ -0,0 +1,27 @@
+#3주차 과제
+
+#1번
+a = input()
+b = input()
+
+a = int(a)
+b = int(b)
+
+print(a+b, a-b, a*b, a/b, a%b)
+
+#2번
+score = int(input())
+
+if(90<= score <= 100): print("A")
+elif(80<=score<=89): print("B")
+elif(70<= score <= 79): print("C")
+elif(60<= score <= 69): print("D")
+else: print("F")
+
+#3번
+num = [1,2,3,4,5,6,7,8,9,10]
+print(num[0::2])
+
+#4번
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "쏘세지", "라면", "팥빙수", "김치전"]
+print(len(cook))
