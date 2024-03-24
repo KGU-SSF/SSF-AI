663d4979d5bfb79c5003921dfcd17d9a77f1a0d6
Moonjunho1121 <moonjunho2003@gmail.com>
2024-03-24 11:11:41 +0900

    Week 3: 문준호 과제 제출

---
+++ 문준호/3주차/Test0301.py
@@ -0,0 +1,7 @@
+numA, numB = map(int, input("두 수를 공백으로 구분하여 입력하시오 : ").split())
+
+print(numA + numB)
+print(numA - numB)
+print(numA * numB)
+print(numA / numB)
+print(numA % numB)
---
+++ 문준호/3주차/Test0302.py
@@ -0,0 +1,13 @@
+score = int(input("시험 점수를 입력하시오 :"))
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
+1
