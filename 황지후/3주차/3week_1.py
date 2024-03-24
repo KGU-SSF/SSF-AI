2514b93db860b29df2ee4dbd531c2c4b3e99459e
jiwhoo0414 <jiwho.hwang@gmail.com>
2024-03-24 22:44:26 +0900

    Week 3 : 황지후 과제제출

--- 황지후/3주차/3week_1.py
+++ 황지후/3주차/3week_1.py
@@ -2,4 +2,4 @@ A = int(input("A값을 입력하시오 : "))
 B = int(input("B값을 입력하시오 : "))
 
 print ("{} {} {} {} {}".format(
-    (A + B), (A - B), (A * B), (A / B), (A % B)))
+    (A + B), (A - B), (A * B), int(A / B), (A % B)))
