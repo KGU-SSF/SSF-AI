c273f053d66f603fa76ab6b0312fd97d4a5828d3
sooddui <ddui0@naver.com>
2024-03-24 22:48:01 +0900

    Week 3 : 김지수 과제 제출

--- 김지수/3주차/3-2.py
+++ 김지수/3주차/3-2.py
@@ -4,9 +4,9 @@ if score > 100 or score < 0 :
     print("다시 입력해주세요")
 elif score >= 90 :
     print("A")
-elif score >= 80  :
+elif score >= 80 :
     print("B")
-elif score >= 70:
+elif score >= 70 :
     print("C")
 elif score >= 60 :
     print("D")
