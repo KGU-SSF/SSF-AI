ca3c4f050fa424cc2287a58c36b7f9470c5bed3f
onestonel <onestone0208@gmail.com>
2024-03-28 19:49:42 +0900

    Week 4 : 이원석  과제 제출

--- 이원석/1주차/1-1.py
+++ 이원석/1주차/1-1.py
@@ -1,1 +1,41 @@
-print('hello world!')
+"""i = input("별의 최대 갯수를 입력하세요")
+while i>0:
+    print("{0:>10}".format("*"))
+    i = i - 1
+
+"""
+"""s = 'Welcome to Python'
+print(s.split())
+s = '2023.03.29'
+print(s.split('.'))
+s = 'Hello, World!'
+print(s.split(','))"""
+
+"""s = '***'
+print(s.strip('#'))
+print(s.lstrip('#'))
+print(s.rstrip('#'))
+print(s.strip('#').capitalize())"""
+
+"""import numpy as np
+a = np.array(range(1, 11))
+b = np.array(range(10, 101, 10))
+c = a + b
+# Propeties of ndarray
+print(c.shape) # dimensions of the form (m, n)
+print(c.ndim) # the number of the dimension
+print(c.dtype) # type of the object
+print(c.itemsize) # size of an element based on byte
+print(c.size) # the number of the elements"""
+
+"""import numpy as np
+heights = np.array([1.83, 1.76, 1.69, 1.86, 1.77, 1.73]
+)
+weights =  np.array([86, 74, 59, 95, 80, 68])
+print("bmi= ",weights/(heights*heights))"""
+
+
+
+    
+    
+
