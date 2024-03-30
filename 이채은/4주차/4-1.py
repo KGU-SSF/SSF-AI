f925a22921b16b028ae619747cde85b3ff2ecede
axurxz <axurxz@gmail.com>
2024-03-30 22:59:15 +0900

    Update 4-1.py

--- 이채은/4주차/4-1.py
+++ 이채은/4주차/4-1.py
@@ -1,3 +1,5 @@
-a = ['*', '**', '***', '****', '*****']
-for i in a:
-    print(i.rjust(5))
+x = int(input("줄 수를 입력하세요"))
+for i in range(1, x+1):
+     for j in range(i):
+        print("*", end="")
+     print()
