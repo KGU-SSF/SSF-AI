4ba6c5315b57397be130049f00dc7c46d3b7fbef
leehaksal <geoffry03@naver.com>
2024-03-31 01:51:47 +0900

    Week4: 이학준 과제 제출

--- 이학준/1주차/4주차/Week4.py
+++ 이학준/1주차/4주차/Week4.py
@@ -1,7 +1,31 @@
-marks = [90, 25, 67, 45, 80]
-number = 0
-for mark in marks:
-number = number +1
-if mark < 60:
-continue
-print("%d번 학생 축하합니다. 합격입니다. " % number)
+#4-1
+N = int(input())
+for a in range(1, N+1):
+    print(" " * (N-a)+ "*" * a)   
+
+
+
+#4-2
+price_list = [32100, 32150, 32000, 32500]
+for a in range(len(price_list)):
+    print(price_list[a])
+
+
+
+#4-3
+def print_coin():
+    print("비트코인")
+
+print_coin()
+
+
+
+#4-4
+for a in range(0,31,3):
+    print(a)
+
+
+
+#4-5
+a = tuple(range(2,100,2))
+print(a)
