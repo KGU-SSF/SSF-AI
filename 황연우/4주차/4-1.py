a988c91756193b2b5c8ec6594af183e95724760d
황연우 <162822179+hooohooohhoo@users.noreply.github.com>
2024-04-01 00:58:31 +0900

    Delete 황연우/4주차 directory

--- 황연우/4주차/4-1.py
+++
@@ -1,5 +0,0 @@
-N = int(input("N을 입력하시오: "))
-
-for num in range(1,N+1):
-    print(" "*(N-num))
-    print("*"*num)
--- 황연우/4주차/4-2.py
+++
@@ -1,4 +0,0 @@
-price_list = [32100,32150,32000,32500]
-
-for num in range(len(price_list)):
-    print(price_list[1])
