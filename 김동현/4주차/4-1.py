d7d26f54d7022c53a3b5d8be652cdcfbacdb74b5
Pr8toT <dothebest258@gmail.com>
2024-04-01 00:42:21 +0900

    Week 4 : 김동현 과제 제출

--- 김동현/4주차/4-1.py
+++ 김동현/4주차/4-1.py
@@ -10,3 +10,4 @@ a = n = int(input())
 for n in range(1, n + 1):
     print(("*"*n).rjust(a)) #문자열.rjust(자릿수, 채울 것) 오른쪽 정렬
     n = n + 1
+    #commit 메세지 수정용
--- 김동현/4주차/4-3.py
+++ 김동현/4주차/4-3.py
@@ -2,3 +2,4 @@ def print_coin():
     print("비트코인")
     
 print_coin()
+#commit 메세지 수정용
