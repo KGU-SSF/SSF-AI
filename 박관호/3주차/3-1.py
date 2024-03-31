3c4e969f588b6e097bcf4f2730dd03547d78bf98
CatEater00 <fkzpt345@gmail.com>
2024-03-31 17:21:09 +0900

    Update 3-1.py

--- 박관호/3주차/3-1.py
+++ 박관호/3주차/3-1.py
@@ -1,7 +1,7 @@
 while 1 :
     x = int(input("정수 x를 입력하시오."))
     y = int(input("정수 y를 입력하시오."))
-    if x < 0 and y < 0 :
+    if x <= 0 and y <= 0 :
         print("자연수가 아닙니다")
         continue
     else :
