cc1670285a8db7958a183930bfd692c9ec11fe11
Ye-eunnn <poppy02460@naver.com>
2024-04-01 13:30:13 +0900

    week 3: 김예은 과제 제출

---
+++ 김예은/3주차/3-1.py
@@ -0,0 +1,14 @@
+num1 = int(input("값을 입력하시오 : " ))
+num2 = int(input("값을 입력하시오 : " ))
+
+rst1 = num1 + num2
+rst2 = num1 - num2
+rst3 = num1 * num2
+rst4 = num1 / num2
+rst5 = num1 % num2
+
+print(int(rst1))
+print(int(rst2))
+print(int(rst3))
+print(int(rst4))
+print(int(rst5))
