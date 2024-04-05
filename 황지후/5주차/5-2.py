d85499ebcef47d81971afb7a26c14630b064c52e
jiwhoo0414 <jiwho.hwang@gmail.com>
2024-04-05 09:25:18 +0900

    Week 5 : 황지후 과제제출 코드설명작성

--- 황지후/5주차/5-2.py
+++ 황지후/5주차/5-2.py
@@ -1,4 +1,7 @@
+#N값을 받아오기
 N = int(input("N값을 입력하시오 : "))
+
+#N번만큼 *_로 피라미드층을 만들고 가운대에 정렬 후 각 층을 출력하기
 for i in range (N):
     stars = ("* " * (i + 1)).center(N*2, " ")
     print(stars)
--- 황지후/5주차/5-3.py
+++ 황지후/5주차/5-3.py
@@ -1,9 +1,12 @@
+#윷 정보를 받아와 공백을 기준으로 각 값으로 나눠 리스트에 넣기
 pla_list = map(int, input("윷 정보를 입력하시오 : ").split())
 pla_list = list(pla_list)
 pla_sum = 0
+#0 과 1로 저장된 윷의 상태를 모두 더하기
 for i in range(4):
     pla_sum += pla_list[i]
 
+#윷의 상태를 더한 값을 바탕으로 족보에 따라 결과 내보내기
 if pla_sum == 3:
     print ("A")
 elif pla_sum == 2:
