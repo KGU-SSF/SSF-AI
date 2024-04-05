fed311684eece9714d48a8a367f8073a20bff55a
ryujihos0105 <ryujihos0105@gmail.com>
2024-04-05 20:03:51 +0900

    Week 5: 류지호 과제 제출
    5-1, 5-2 코드 수정하였습니다

--- 류지호/5주차/5-1.py
+++ 류지호/5주차/5-1.py
@@ -1,17 +1,16 @@
-a=int(input("준서의 성능은?"))
-if a<50:
-    a=50
-b=int(input("이다의 성능은?"))
-if b<50:
-    b=50 
-c=int(input("홍다의 성능은?"))
-if c<50:
-    c=50
-d=int(input("우진의 성능은?"))
-if d<50:
-    d=50        
-e=int(input("우현의 성능은?"))
-if e<50:
-    e=50 
-print((a+b+c+d+e)/5) 
-      
+sum=0                                           
+name=["준서","이다","홍다","우진","우현"]     #이름 리스트로 하나씩 출력
+for i in (name):                             #이름 개수만큼 반복
+    while True:                              
+        a=int(input(i+"의 성능:"))             #성능을 입력받고 그 값이 0부터 100사이, 5의 배수가 나올떄 까지 반복
+        if a>100 or a<0:
+            print("0과100사이 입력")
+        elif a %5 !=0:
+            print("5의 배수 입력")
+        else:
+            break
+    if a<50:                                 #a의 값이 50이 안되면 50으로 저장
+        a=50                  
+    sum+=a                                    #a의 값을 sum에 더하면서 저장
+print('5명의 평균:', sum/5)                     #5명의 평균 출력             
+    
--- 류지호/5주차/5-2.py
+++ 류지호/5주차/5-2.py
@@ -1,3 +1,6 @@
-a=int(input("줄의 갯수: "))
-for i in range(a):
-    print(" "*(a-i-1)+"*"+(" "+"*")*i)
+while True:                          #줄의 개수를 1부터 100사이로 입력받기
+    a=int(input("줄의 개수: "))
+    if 1<=a<=100:
+        break 
+for i in range(a):                    #줄의 개수 만큼 반복
+    print(" "*(a-i-1)+"*"+(" "+"*")*i)   # 각 줄에 해당하는 별의 개수와 공백 출력
