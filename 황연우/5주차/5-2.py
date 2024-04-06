ce96e43b4e1cf37764b73cd3278f682b262866a3
황연우 <162822179+hooohooohhoo@users.noreply.github.com>
2024-04-07 00:56:30 +0900

    Add files via upload

---
+++ 황연우/5주차/5-2.py
@@ -0,0 +1,20 @@
+a=int(input("성능을 입력하시오: "))
+b=int(input("성능을 입력하시오: "))
+c=int(input("성능을 입력하시오: "))
+d=int(input("성능을 입력하시오: "))
+e=int(input("성능을 입력하시오: "))
+
+if a<50:
+    a = 50
+if b<50:
+    b = 50
+if c<50:
+    c = 50
+if d<50:
+    d = 50
+if e<50:
+    e = 50
+    
+print(int((a+b+c+d+e)/5))                                    
+        
+    
