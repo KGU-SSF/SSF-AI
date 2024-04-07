2c635566a7f4aa970a237de90fa23b3be9c9a7c8
[oyeo048] <[oyeo048@naver.com]>
2024-04-07 16:29:15 +0900

    Week 5 : 최현빈 과제 제출

--- 최현빈/5주차/5-2.py
+++ 최현빈/5주차/5-2.py
@@ -1,9 +1,9 @@
-n = int(input())
+n = int(input())                 #n 입력
 
-for i in range(1, n+1):
-    for j in range(0, n-i):
+for i in range(1, n+1):          #출력할 층수 결정
+    for j in range(0, n-i):      #공백 출력
         print(" ", end = "")
-    for k in range(0, i):
+    for k in range(0, i):        #별 출력
         print("* ", end = "")
         
-    print("")
+    print("")                    #줄바꿈
