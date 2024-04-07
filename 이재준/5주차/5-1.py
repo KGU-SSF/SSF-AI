a80804703f3d33ddbdffd0508e4ac7a5c265778d
ljjisgood <87285784+Osori-ramen@users.noreply.github.com>
2024-04-07 14:40:05 +0900

    Update 5-1.py

--- 이재준/5주차/5-1.py
+++ 이재준/5주차/5-1.py
@@ -2,7 +2,7 @@
 이다 = int(input())
 홍다 = int(input())
 우진 = int(input())
-우현 = int(input())
+우현 = int(input())  #수 입력받기
 if 준서<50:
   준서=50
 if 이다<50:
@@ -15,5 +15,5 @@ if 우현<50:
   우현=50
 print(준서,이다,홍다,우진,우현)
 a=준서+이다+홍다+우진+우현
-b=a/5
+b=a/5                      #나눗셈
 print(b)
