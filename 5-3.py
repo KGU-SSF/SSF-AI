709d760169be006406f06d67ce947edf03872ae1
jjeong47 <166278309+jjeong47@users.noreply.github.com>
2024-04-08 00:47:43 +0900

    Add files via upload

---
+++ 5-3.py
@@ -0,0 +1,15 @@
+#3. 윷놀이 결과 출력 프로그램
+#등:1 배:0
+x=input()
+count= x.count('1')
+if count==0:
+    print('D')
+elif count==1:
+    print('C')
+elif count==2:
+    print('B')
+elif count==3:
+    print('A')
+elif count==4:
+    print('E')
+    
