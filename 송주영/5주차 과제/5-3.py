f1136f07db1a27bd42c914556837509189820a54
judangyee <jooy.song05@gmail.com>
2024-04-07 20:47:08 +0900

    week5: 송주영 과제 제출

--- 송주영/5주차 과제/5-3.py
+++ 송주영/5주차 과제/5-3.py
@@ -1,7 +1,7 @@
 # 결과 출력 함수
-def determine_result(results):
-    b = results.count(0) #배
-    d = results.count(1) #등
+def determine_result(results): #규칙에 따라 결과 결정
+    b = results.count(0) #배 0의개수 할당
+    d = results.count(1) #등 1의개수 할당
     
     if b == 1 and d == 3:
         return "A"
@@ -17,10 +17,10 @@ def determine_result(results):
         return "잘못된 입력!"
 
 # 입력 받기
-results = []
+results = [] #리스트 생성
 for _ in range(3):
     line = input().split()
-    results.append([int(x) for x in line])
+    results.append([int(x) for x in line]) #리스트에 추가
 
 # 결과 출력
 for result in results:
