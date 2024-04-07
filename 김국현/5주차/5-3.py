70e2d40290f262529da56484ede2d66ddc93a996
guk4979 <guk5871@gmail.com>
2024-04-07 23:45:54 +0900

    Week 5 : 김국현 과제 제출

---
+++ 김국현/5주차/5-3.py
@@ -0,0 +1,11 @@
+sum = 0
+
+for i in range(4):
+    a = int(input())
+    sum += a
+
+if(a == 3):print("A")
+if(a == 2):print("B")
+if(a == 1):print("C")
+if(a == 0):print("D")
+if(a == 4):print("E")
