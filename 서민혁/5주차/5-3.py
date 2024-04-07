b00b28088e6d70b471388eb72ae7b5f50f2ea92a
Seominhyeok05 <164611679+Seominhyeok05@users.noreply.github.com>
2024-04-07 21:39:48 +0900

    Update 5-3.py

--- 서민혁/5주차/5-3.py
+++ 서민혁/5주차/5-3.py
@@ -1,14 +1,6 @@
 a = []
 for i in range(3):
     a.append(input().split())
+b=['D','C','B','A','E']
 for i in range(3):
-    if a[i].count('1') == 0:
-        print("D")
-    elif a[i].count('1') == 1:
-        print("C")
-    elif a[i].count('1') == 2:
-        print("B")
-    elif a[i].count('1') == 3:
-        print("A")
-    elif a[i].count('1') == 4:
-        print("E")
+    print(b[a[i].count('1')])
