4e86f490c3a0c95c6fd2c8e1d92417211c0f53cc
alswjd125 <kmj2830125@naver.com>
2024-04-07 14:16:52 +0900

    week 5: 김민정 과제 제출

--- 김민정/5주차/5-2.py
+++ 김민정/5주차/5-2.py
@@ -1,5 +1,8 @@
 N = int(input())
+star= "* "
+space=" "
 
-for i in range(N):
+for i in range(N, 0 , -1):
     
-    print(" " * (N-i) + "*" * (i*2+1))
+    print(space *  (i -1), end=" ")
+    print(star * (N-(i-1)))
