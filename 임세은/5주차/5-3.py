5ae83bfa577839c57df25371b063fb0400f48983
limseeeun <limseeun566@gmail.com>
2024-04-07 17:47:42 +0900

    Week 5:임세은 과제 제출

--- 임세은/5주차/5-3.py
+++ 임세은/5주차/5-3.py
@@ -1,15 +1,15 @@
-arr = [[0] * 4 for _ in range(3)]
+arr = [[0] * 4 for _ in range(3)]   #3행 4열의 배열 선언 및 초기화
 
 for i in range(3):
-    arr[i]=list(map(int,input().split()))
+    arr[i]=list(map(int,input().split()))   #split를 사용하여 4개의 값입력받기
 
-result=[0]*3
+result=[0]*3    #결과를 입력할 배열 선언 및 초기화
 
-for i in range(3):
+for i in range(3):  #result배열에 arr 각행의 값을 저장
     for k in range(4):
         result[i]+=arr[i][k]
 
-for i in range(3):
+for i in range(3):  #result배열의 값에 따른 결과값 출력0
     if result[i]==0:
         print("D")
 
