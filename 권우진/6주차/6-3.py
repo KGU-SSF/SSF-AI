62f6aa2013aa8d5a7b50b3b7c6bad1e445d66470
kwj0175 <kwj075@gmail.com>
2024-04-08 22:59:40 +0900

    Week 6 : 권우진 과제 제출

--- 권우진/6주차/6-3.py
+++ 권우진/6주차/6-3.py
@@ -8,12 +8,12 @@ for i in range(n):
     queue.append(int(input("숫자 입력: ")))
     
 for i in range(len(queue)):
-    min = i
+    max = i
     for j in range(i + 1, len(queue)):
-        if queue[min] < queue[j]:
-            min = j
-    tmp = queue[min]
-    queue.remove(queue[min])
+        if queue[max] < queue[j]:
+            max = j
+    tmp = queue[max]
+    queue.remove(queue[max])
     queue.appendleft(tmp)
 
 for i in range(len(queue)):
