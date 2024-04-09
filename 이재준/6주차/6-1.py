2165e6ee1dd807392bc7055f5641de79e845bf15
ljjisgood <87285784+Osori-ramen@users.noreply.github.com>
2024-04-09 17:31:16 +0900

    Update 6-1.py

--- 이재준/6주차/6-1.py
+++ 이재준/6주차/6-1.py
@@ -1,11 +1,11 @@
 def name(word):
-    return word == word[::-1] #문자열과 뒤집은 문자열이 같은가?
+    return word == word[::-1] #문자열과 뒤집은 문자열이 같은지 확인하는 함수,[::-1]은 문자열을 거꾸로 뒤집는 역할을 한다.
 
-def check(word):
-    if name(word):
+def check(word):         #word가 회문인지 확인해보자
+    if name(word):        
         print("True")     #같다
     else:
         print("False")   #아니다
 
-word = input("입력 예시: ")
+word = input("입력 예시: ") #문자열 입력받기
 check(word)
