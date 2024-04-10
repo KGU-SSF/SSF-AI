3ecf10bc438d42c411afb387a1efda0f6c9f09ac
heoyeonbeom <161940170+heoyeonbeom@users.noreply.github.com>
2024-04-10 20:46:04 +0900

    Wek 6 : 허윤범 과제 제출

--- 허윤범/6주차/6-2.py
+++ 허윤범/6주차/6-2.py
@@ -1,16 +1,16 @@
-K = int(input())  # 카드의 개수 입력
+K = int(input())  # 카드의 개수를 입력받는다
 
-stack = []  # 스택 초기화
-total = 0   # 카드에 적힌 숫자의 합을 저장할 변수
+stack = []  # 스택을 초기화 한다
+t = 0   
 
-for _ in range(K):
-    num = int(input())  # 각 카드에 적힌 숫자 입력
+for _ in range(K):  #K번 반복한다
+    num = int(input()) 
     
     if num == 0:
-        removed = stack.pop()  # 가장 최근에 말한 카드를 스택에서 제거
-        total -= removed  # 제거한 카드의 숫자를 합에서 뺌
+        d = stack.pop()  # 가장 최근에 말한 카드를 스택에서 제거한다
+        t -= d  # 제거한 카드의 숫자를 합에서 뺸다
     else:
-        stack.append(num)  # 카드 숫자를 스택에 추가
-        total += num  # 카드 숫자를 합에 더함
+        stack.append(num)  # 카드 숫자를 스택에 추가한다
+        t += num  # 카드 숫자를 합에 더한다
 
-print(total)  # 카드에 적힌 숫자의 합 출력
+print(t)  # 카드에 적힌 숫자의 합 출력한다
