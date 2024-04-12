60f047e2e8f210a603f08a7e72afaa37104ff419
hyunjin1229 <hyunjin24@kyonggi.ac.kr>
2024-04-12 19:12:19 +0900

    Week 6: 장현진 과제 제출

--- 장현진/6주차/6-2.py
+++ 장현진/6주차/6-2.py
@@ -1,11 +1,11 @@
 stack = []
-a = int(input())
-for i in range(a):
-    b = int(input())
-    if b != 0:
+a = int(input())#사용자로부터a 입력받음
+for i in range(a):#0부터 a-1까지 반복
+    b = int(input())#사용자로부터 b입력받음
+    if b != 0:#b가 0이 아닌경우 stack에 추가
         stack.append(b)
-    elif b == 0:
+    elif b == 0:#b가 0인경우 stack에서 맨 위값 제거
         stack.pop()
-s = sum(stack)
+s = sum(stack)#반복문 종료 후 남아 있는값 합 s에 할당
 print()         
 print(s)
