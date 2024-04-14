e50aedc5260f30d97635c7c862772f0bc0b1bbf3
dabbnn <danna0508@gmail.com>
2024-04-14 20:41:27 +0900

    week 6 : 임단아 과제제출

--- 임단아/6주차/6-1.py
+++ 임단아/6주차/6-1.py
@@ -1,9 +1,9 @@
 #6주차 1번
 
-words = ['level','rotator','hello' ]
+words = ['level','rotator','hello' ]    # 단어출력
 
 for word in words:
-    if list(word) == list(reversed(word)):     # 역순과 비교
-        print(word, "TRUE")
+    if list(word) == list(reversed(word)):     # 역순 비교하기
+        print(word, "TRUE")     # 역순이면 true출력
     else:
-        print(word, "FALSE")
+        print(word, "FALSE")    #역순이 아니면 false출력
--- 임단아/6주차/6-3.py
+++ 임단아/6주차/6-3.py
@@ -3,7 +3,7 @@
 n = int(input("숫자를 입력하시오. : "))   #값 입력
 num_list=[]
 for i in range(n):
-    num_list.append(int(input()))   #값 입력을 반복
+    num_list.append(int(input()))   #값을 n번 num_list에 입력받기
 num_list.sort()
 for i in range(n):
     print(num_list[i])  #입력한 값을 오름차순으로 정렬하여 출력
