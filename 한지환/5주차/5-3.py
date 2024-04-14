aa19f3e318d8448bcfb2b58bae3c2566fd4eae67
hanjihwan18 <162652769+hanjihwan18@users.noreply.github.com>
2024-04-14 17:10:53 +0900

    Week 5: 한지환 과제 제출

--- 한지환/5주차/5-3.py
+++ 한지환/5주차/5-3.py
@@ -1,1 +1,15 @@
 #3번문제
+for i in range(3):
+    a = list(map(int,input().split()))
+    zero = a.count(0)
+
+    if zero == 1:
+        print("A")
+    elif zero == 2:
+        print("B")
+    elif zero == 3:
+        print("C")
+    elif zero == 4:
+        print("D")
+    else : 
+        print("E")
