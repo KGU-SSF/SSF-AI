0c187119b7d598e179be644ba445f7f48a764944
guk4979 <guk5871@gmail.com>
2024-04-14 23:10:32 +0900

    Week 6 : 김국현 과제 제출

---
+++ 김국현/6주차/6-1.py
@@ -0,0 +1,6 @@
+strings = list(input())
+temp = list(reversed(strings))
+
+if strings == temp:
+    print("True")
+else: print("False")
---
+++ 김국현/6주차/6-3.py
@@ -0,0 +1,8 @@
+count = int(input())
+nums = []
+for i in range(count):
+    nums.append(int(input()))
+
+print(sorted(nums))
+
+    
