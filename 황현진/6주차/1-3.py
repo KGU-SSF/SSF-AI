770df0ae14d1a27e1723d4d80076d863befbaaec
ghkd9425naver.com <ghkd9425@naver.com>
2024-04-14 14:45:12 +0900

    Week 6: 황현진 과제제출

--- 황현진/6주차/1-3.py
+++ 황현진/6주차/1-3.py
@@ -1,7 +1,8 @@
-a = int(input("몇개의 숫자를 입력하실건가요? : "))
-number = []
-for i in range(0, a):
-    b = int(input())
+a = int(input("몇개의 숫자를 입력하실건가요? : ")) #입력받을 숫자의 개수를 입력받는다 
+number = []             
+for i in range(0, a):                            #반복문을 이용하여 a번만큼 숫자를 입력받고 number리스트에 추가한다
+    b = int(input())                             #이후 오름차순을 해주는 sort함수를 이용하여 결과값을 출력한다
     number.append(b)
 number.sort()
 print(number)
+
