00a23fc1854578081bc1fc6cc0b3f9ab557cf527
[oyeo048] <[oyeo048@naver.com]>
2024-04-14 21:50:41 +0900

    Week 6 : 최현빈 과제 제출

--- 최현빈/6주차/6-3.py
+++ 최현빈/6주차/6-3.py
@@ -1,10 +1,10 @@
 n = int(input())
-a = []
+a = []                     #빈 리스트 생성
 for i in range(n):
-    b = int(input())
-    a.append(b)
+    b = int(input())       #정수 입력 받아 b에 저장
+    a.append(b)            #b를 a에 저장
 
-a.sort()
+a.sort()                   #리스트에 저장된 정수들을 오름차순으로 정리
 
 for b in a:
-    print(b)
+    print(b)               #출력
