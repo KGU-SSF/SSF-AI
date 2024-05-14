34cf67ddf35cacaa69ee79779843c949750b2275
limseeeun <limseeun566@gmail.com>
2024-05-14 22:31:59 +0900

    Week 11: 임세은 과제제출

--- 임세은/11주차/11-1.py
+++ 임세은/11주차/11-1.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-input_data=np.array([3,5])
-weight_hidden_0=np.array([2,3]) 
-weight_hidden_1=np.array([4,-5])
-weight_output=np.array([2,7])
+input_data=np.array([3,5])  
+weight_hidden_0=np.array([2,3])    
+weight_hidden_1=np.array([4,-5])   
+weight_output=np.array([2,7])  
 
 hidden_0_value=(input_data*weight_hidden_0).sum()
 hidden_1_value=(input_data*weight_hidden_1).sum()
