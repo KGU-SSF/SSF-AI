9c1e6d36454a7bfaa072b6ab9af5ec4e72f20550
Pr8toT <dothebest258@gmail.com>
2024-05-14 19:03:33 +0900

    Week 11 : 김동현 과제 제출

--- 김동현/11주차/11-1.py
+++ 김동현/11주차/11-1.py
@@ -3,7 +3,7 @@ import numpy as np
 input_data = np.array([3,5])
 
 weight_hidden_0 = np.array([2,3])
-weight_hidden_1 = np.array([4,5])
+weight_hidden_1 = np.array([4,-5])
 weight_output = np.array([2,7])
 
 hidden_0_value = (input_data*weight_hidden_0).sum()
