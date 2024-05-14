a19a05085017e6b70d074e2fc9887ec5ccf06d54
ljjisgood <87285784+Osori-ramen@users.noreply.github.com>
2024-05-14 19:11:15 +0900

    Update 행렬.py

--- 이재준/11주차/행렬.py
+++ 이재준/11주차/행렬.py
@@ -6,7 +6,7 @@ weight_hidden_1 = np.array([4, -5])   #두번 째 은닉층 가중치
 weight_output = np.array([2, 7])      #세 번째 은닉층 가중
 
 hidden_0_value = (input_data * weight_hidden_0).sum() 
-hidden_1_value = (input_data * weight_hidden_1).sum()   #은닉층 값계
+hidden_1_value = (input_data * weight_hidden_1).sum()   #은닉층 값계산
 print(hidden_0_value, hidden_1_value, sep="")            #은닉층 값 출력
 output = hidden_0_value* weight_output[0] + hidden_1_value* weight_output[1]   #출력층 값 계산
 print(output)                                                             #출력층 값 출력
