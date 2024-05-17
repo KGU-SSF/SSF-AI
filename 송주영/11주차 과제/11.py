095430352ce813e8a3326714ad216b896267eba6
judangyee <jooy.song05@gmail.com>
2024-05-18 00:25:16 +0900

    Week11: 송주영 과제 제출

--- 송주영/11주차 과제/11.py
+++ 송주영/11주차 과제/11.py
@@ -1,10 +1,10 @@
 import numpy as np 
 
-input_data = np.array([3,5])
-weight_hidden_0 = np.array([2,3])
-weight_hidden_1 = np.array([4,-5])
-weight_output = np.array([2,7])
-
+input_data = np.array([3,5]) #입력 값
+weight_hidden_0 = np.array([2,3]) #입력값 가중치
+weight_hidden_1 = np.array([4,-5]) #입력값 가중치 
+weight_output = np.array([2,7]) #출력값 가중치
+#행렬곱 연산
 hidden_0_value = (input_data * weight_hidden_0).sum()
 hidden_1_value = (input_data * weight_hidden_1).sum()
 print(hidden_0_value, hidden_1_value, sep=',')
