751d41ac9467d45e730c6119abd9eb9dbfc2135a
cistus75 <cistus75@gmail.com>
2024-05-20 15:11:03 +0900

    week11 : 최재우 과제 제출

--- 최재우/11주차/11-1.py
+++ 최재우/11주차/11-1.py
@@ -1,14 +1,14 @@
-import numpy as np
+import numpy as np #넘파이 받아오기 
 
-input_data = np.array([3, 5])
+input_data = np.array([3, 5]) #초기 입력값 정의
 
-weight_hidden_0 = np.array([2, 3])
-weight_hidden_1 = np.array([4, -5])
-weight_output = np.array([2, 7])
+weight_hidden_0 = np.array([2, 3]) #1번째 입력값 가중치
+weight_hidden_1 = np.array([4, -5]) #2번째 입력값 가중치
+weight_output = np.array([2, 7]) #hidden_0의 가중치가 전자, hidden_1의 가중치가 후자
 
-hidden_0_value = (input_data * weight_hidden_0).sum()
-hidden_1_value = (input_data * weight_hidden_1).sum()
-print(hidden_0_value, hidden_1_value, sep=",")
+hidden_0_value = (input_data * weight_hidden_0).sum() #계산 값은 21
+hidden_1_value = (input_data * weight_hidden_1).sum() #계산2 값은 -13
+print(hidden_0_value, hidden_1_value, sep=",") #위의 계산값 2개 출력
 
-output = hidden_0_value * weight_output[0] + hidden_1_value * weight_output[1]
-print(output)
+output = hidden_0_value * weight_output[0] + hidden_1_value * weight_output[1] #value와 가중치값을 계산
+print(output) #최종결과 -49 출력
