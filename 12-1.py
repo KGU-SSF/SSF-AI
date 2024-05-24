9f7abad6f87ddb13ea6ae9661e2622ce55c8a4c2
sehyeonkim7565 <162868544+sehyeonkim7565@users.noreply.github.com>
2024-05-24 18:54:45 +0900

    Week 12:김세현 과제 제출

---
+++ 12-1.py
@@ -0,0 +1,20 @@
+input_data=5 #입력 받는 값
+weight=3 #가중치
+target=8 #목표하는 값
+learning_rate=0.1 #학습 빈도, 가중치 조절
+
+pred=weight*input_data #가중치*입력 받는 값=예측값
+error=pred-target #예측값-목표하는 값=오차
+print("original error:", error)
+
+slope=2*input_data*error #2*입력 받는 값*오차=오차함수의 기울기
+print("slope:",slope)
+
+new_weight=weight-learning_rate*slope #가중치-학습 빈도*기울기=새로운 가중치
+print("new weight:",new_weight)
+
+new_pred=new_weight*input_data #새로운 가중치*입력 받는 값=새로운 예측값
+print("new prediction:", new_pred)
+
+new_error=new_pred-target #새로운 예측값-목표 하는 값=새로운 오차
+print("new error:", new_error)
