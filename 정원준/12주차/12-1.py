953159e94defc9b80e257b92bb789110f85930a6
good04brain <162821776+good04brain@users.noreply.github.com>
2024-05-25 12:38:42 +0900

    Week 12 : 정원준 과제 제출
    Week 12 : 정원준 과제 제출

---
+++ 정원준/12주차/12-1.py
@@ -0,0 +1,20 @@
+input_data=5
+weight=3    #가중치
+target=8    #실제값
+learning_rate=0.1
+
+pred=weight*input_data  #예측값
+error=pred - target     #오차
+print("original error:", error)
+
+slope=2*input_data*error    #기울기
+print("slope:", slope)
+
+new_weight=weight-learning_rate*slope   #새로운 가중치
+print("new weight:", new_weight)
+
+new_pred=new_weight*input_data  #새로운 예측값
+print("new prediction:", new_pred)
+
+new_error=new_pred-target   #새로운 오차
+print("new error:", new_error)
