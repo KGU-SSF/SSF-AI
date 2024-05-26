20fabe4d7087b4cae7918a2921a46b77775cc886
ljjisgood <87285784+Osori-ramen@users.noreply.github.com>
2024-05-27 01:00:39 +0900

    Create 12-1.py

---
+++ 이재준/12주차/12-1.py
@@ -0,0 +1,20 @@
+input_data=5  #데이터 입력
+weight=3    #가중치
+target=8    #실제값
+learning_rate=0.1 #학습률 입력
+
+pred=weight*input_data  #예측값＝가중치＊초기값
+error=pred - target     #오차＝예측값－실제값
+print("original error:", error)  #에러 출력
+
+slope=2*input_data*error    #오차함수기울기＝２＊초기값＊오차
+print("slope:", slope)       #기울기 출력
+
+new_weight=weight-learning_rate*slope   #새로운 가중치＝가중치　－학습률＊기울기
+print("new weight:", new_weight)    #new weight값 출력
+
+new_pred=new_weight*input_data  #새로운 예측값＝새로운　가중치　＊　초기값
+print("new prediction:", new_pred)  #"new prediction값 출력
+
+new_error=new_pred-target   #새로운 오차＝새로운예측값-실제값
+print("new error:", new_error) #새로운 오차값 출력
