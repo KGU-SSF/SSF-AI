0c22222d7a85b5a1edfc4eb2e70405f9fe1d82fa
good04brain <162821776+good04brain@users.noreply.github.com>
2024-05-26 22:14:18 +0900

    Week 12 : 정원준 과제 제출
    Week 12 : 정원준 과제 제출

--- 정원준/12주차/12-1.py
+++ 정원준/12주차/12-1.py
@@ -3,18 +3,18 @@ weight=3    #가중치
 target=8    #실제값
 learning_rate=0.1
 
-pred=weight*input_data  #예측값
-error=pred - target     #오차
+pred=weight*input_data  #예측값=가중치*입력값
+error=pred - target     #오차=예측값-실제값
 print("original error:", error)
 
-slope=2*input_data*error    #기울기
+slope=2*input_data*error    #오차함수의 기울기=2*입력값*오차
 print("slope:", slope)
 
-new_weight=weight-learning_rate*slope   #새로운 가중치
+new_weight=weight-learning_rate*slope   #새로운 가중치로 업데이트
 print("new weight:", new_weight)
 
-new_pred=new_weight*input_data  #새로운 예측값
+new_pred=new_weight*input_data  #새로운 예측값=새로운 가중치*입력값
 print("new prediction:", new_pred)
 
-new_error=new_pred-target   #새로운 오차
+new_error=new_pred-target   #새로운 오차=새로운 예측값-실제값
 print("new error:", new_error)
