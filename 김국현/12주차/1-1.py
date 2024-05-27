83e051bae091542cfd847d1b5e6ba31800c4d17e
guk4979 <guk5871@gmail.com>
2024-05-28 02:15:19 +0900

    Week 12 : 김국현 과제 제출

--- 김국현/12주차/1-1.py
+++ 김국현/12주차/1-1.py
@@ -4,19 +4,19 @@ target = 8
 
 learning_rate = 0.01
 
-pred = weight * input_data
+pred = weight * input_data  #예측
 
-error = pred - target
+error = pred - target       #오차 구하기
 print("original error:", error)
 
-slope = 2 * input_data * error
+slope = 2 * input_data * error  #기울기 구하기
 print("slope:", slope)
 
-new_weight = weight - learning_rate * slope
+new_weight = weight - learning_rate * slope #새로운 가중치 구하기
 print("new weight:",new_weight)
 
-new_pred = new_weight*input_data
+new_pred = new_weight*input_data    #새 예측 구하기
 print("new prediction:", new_pred)
 
-new_error = new_pred - target
+new_error = new_pred - target       #새 오차 구하기
 print("new error:", new_error)
