9a571f73be7bb7cce452f5029f13cab73eaadd84
sehyeonkim7565 <162868544+sehyeonkim7565@users.noreply.github.com>
2024-05-28 00:15:37 +0900

    Week 12 : 김세현 과제 제출
    input_data=5 #입력 받는 값
    weight=3 #가중치
    target=8 #목표하는 값
    learning_rate=0.1 #학습 빈도, 가중치 조절
    
    pred=weight*input_data #가중치*입력 받는 값=예측값
    error=pred-target #예측값-목표하는 값=오차
    print("original error:", error)
    
    slope=2*input_data*error #2*입력 받는 값*오차=오차함수의 기울기
    print("slope:",slope)
    
    new_weight=weight-learning_rate*slope #가중치-학습 빈도*기울기=새로운 가중치
    print("new weight:",new_weight)
    
    new_pred=new_weight*input_data #새로운 가중치*입력 받는 값=새로운 예측값
    print("new prediction:", new_pred)
    
    new_error=new_pred-target #새로운 예측값-목표 하는 값=새로운 오차
    print("new error:", new_error)
    
    리뷰가 인코딩문제로 깨져 보여서 description에 다시 작성하여 제출합니다.

--- 12-1.py
+++ 12-1.py
@@ -1,20 +1,20 @@
-input_data=5 #입력 받는 값
-weight=3 #가중치
-target=8 #목표하는 값
-learning_rate=0.1 #학습 빈도, 가중치 조절
+input_data=5 
+weight=3 
+target=8 
+learning_rate=0.1 
 
-pred=weight*input_data #가중치*입력 받는 값=예측값
-error=pred-target #예측값-목표하는 값=오차
+pred=weight*input_data 
+error=pred-target 
 print("original error:", error)
 
-slope=2*input_data*error #2*입력 받는 값*오차=오차함수의 기울기
+slope=2*input_data*error 
 print("slope:",slope)
 
-new_weight=weight-learning_rate*slope #가중치-학습 빈도*기울기=새로운 가중치
+new_weight=weight-learning_rate*slope 
 print("new weight:",new_weight)
 
-new_pred=new_weight*input_data #새로운 가중치*입력 받는 값=새로운 예측값
+new_pred=new_weight*input_data 
 print("new prediction:", new_pred)
 
-new_error=new_pred-target #새로운 예측값-목표 하는 값=새로운 오차
+new_error=new_pred-target 
 print("new error:", new_error)
