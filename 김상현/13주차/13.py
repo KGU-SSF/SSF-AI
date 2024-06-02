4aca607fc70eb93275e6a340f2e8f91b9f707cad
sanghyunk <0100193@naver.com>
2024-06-03 00:22:56 +0900

    Week 13 : 김상현 과제 제출

--- 김상현/13주차/13.py
+++ 김상현/13주차/13.py
@@ -10,6 +10,7 @@ y_data = diabetes.target
 x_train, x_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=42)
 lr = LinearRegression()
 lr.fit(x_train, y_train)
+
 y_train_pred = lr.predict(x_train)
 r2_train = r2_score(y_train, y_train_pred)
 print(f"r2_train: {r2_train}")
